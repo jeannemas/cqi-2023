7fe521fe97295ecfd91882b24dcdf8ff1cd8e9d6
amDeimos666 <71735806+amDeimos666@users.noreply.github.com>
2023-01-27 00:00:34 -0500

    lost-found page

---
+++ src/lib/components/lost-found/item.svelte
@@ -0,0 +1,59 @@
+<script>
+let modalIsOpen = false;
+
+const openModal = () => (modalIsOpen = true);
+const closeModal = () => (modalIsOpen = false);
+const triggerModal = () => (modalIsOpen = !modalIsOpen);
+/**
+ * @type {String}
+ */
+export let name;
+/**
+ * @type {String}
+ */
+export let desciption;
+/**
+ * @type {String}
+ */
+export let time;
+</script>
+
+<tr>
+  <td>{name}</td>
+  <td>{desciption}</td>
+  <td>{time}</td>
+  <td
+    ><button
+      id="btn"
+      class="btn"
+      on:click="{() => triggerModal()}">Trouvé</button
+    ></td
+  >
+</tr>
+
+<div
+  class="modal"
+  class:is-active="{modalIsOpen}"
+>
+  <div class="modal-background"></div>
+
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Modal title</p>
+
+      <button
+        class="delete"
+        aria-label="close"></button>
+    </header>
+
+    <section class="modal-card-body">
+      <!-- Content ... -->
+    </section>
+
+    <footer class="modal-card-foot">
+      <button class="button is-success"> Save changes </button>
+
+      <button class="button"> Cancel </button>
+    </footer>
+  </div>
+</div>
---
+++ src/routes/lost-found/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+import Item from "../../lib/components/lost-found/item.svelte";
+let items = [
+    {
+      id: 1,
+      name: "Sweetest framework ever",
+      desciption: "Church",
+      time: "This is the content of this note"
+    },
+    {
+      id: 2,
+      name: "Sweetest framework ever",
+      desciption: "Church",
+      time: "This is the content of this note"
+    },
+  ];
+</script>
+
+<div>
+  <h2>Objet perdu</h2>
+  <form>
+    <input
+      type="text"
+      placeholder="nom de objet"
+      class="input input-bordered input-info w-full max-w-xs"
+    />
+  </form>
+  <table>
+    <tr>
+      <th>Nom</th>
+      <th>Description</th>
+      <th>Heure trouvé</th>
+      <th>Remise</th>
+    </tr>
+
+    {#each items as item}
+      <Item name={item.name} desciption={item.desciption} time={item.time}/>
+    {/each}
+  </table>
+
+  <h3>Objet non trouvé?</h3>
+  <a href="/reclaim">Réclamez le</a>
+</div>
