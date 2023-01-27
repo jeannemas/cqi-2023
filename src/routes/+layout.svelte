f9ad088a5f178d5b0618ec32cf6e7f221f345c74
PJBeltran <paul-joseph.beltran.1@ens.etsmtl.ca>
2023-01-27 01:54:07 -0500

    WIP layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,34 @@
 import "@skeletonlabs/skeleton/styles/all.css";
 import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
 import "../app.postcss";
+let showMenu = false;
+
+function toggleMenu() {
+    showMenu = !showMenu;
+}
 </script>
 
-<slot />
+<h3 id="title">Titre</h3>
+
+<nav>
+<button class="btn-menu" on:click={toggleMenu}>
+    Menu
+</button>
+<ul class="menu" class:open={showMenu}>
+    <li>
+        <a href="#Planification">Planification</a>
+    </li>
+    <li>
+        <a href="#Horaire">Horaire</a>
+    </li>
+    <li>
+        <a href="#Objetperdu">Objet Perdu</a>
+    </li>
+    <li>
+        <a href="/report">Rapportage de probleme</a>
+    </li>
+</ul>
+<button>Compte</button>
+</nav>
+
+<slot/>
